d5b1fe94950a11bf6d4b2cab64f5487c8b746e25
Luis Hernandez <hegl030420@itsuruapan.edu.mx>
2026-09-24 20:38:22 -0600

    Refactor Firebase config to use environment variables
    Updated Firebase configuration to use environment variables for sensitive information.

--- constants/firebaseConfig.ts
+++ constants/firebaseConfig.ts
@@ -1,14 +1,14 @@
 // Configuración de Firebase
 // Rellena estos campos con los valores de tu proyecto Firebase (Project settings > General > Your apps > SDK setup and configuration)
 export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCZB6nHkIPH1CqxbiqPRmLrMgHHlHo2R9Y",
-  authDomain: "examen-1cd8a.firebaseapp.com",
-  projectId: "examen-1cd8a",
-  storageBucket: "examen-1cd8a.appspot.com",
-  messagingSenderId: "19622054099",
-  appId: "1:19622054099:web:86d2d8991cf2ffc33e2c26",
-  measurementId: "G-T1R1GP017H",
+  apiKey: process.env.EXPO_PUBLIC_GOOGLE_API_KEY ,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_ID_PROYECT,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE,
+  messagingSenderId: process.env.EXPO_PUBLIC_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_ID_APP,
+  measurementId: process.env.EXPO_PUBLIC_ID_MEASURE,
 
   
-  databaseURL: "https://examen-1cd8a-default-rtdb.firebaseio.com",
+  databaseURL: process.env.EXPO_PUBLIC_DATABASE_URL,
 };
